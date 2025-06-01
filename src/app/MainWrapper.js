2c7f98fdac4ca76c1925ec1d8cfb220bfb97752d
mhmojtaba <mhmojtaba@gmail.com>
2025-06-01 14:23:08 +0330

    [ADD] main wrapper

---
+++ src/app/MainWrapper.js
@@ -0,0 +1,63 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { notFound, usePathname, useRouter } from "next/navigation";
+import { useAuth } from "@/context/AuthContext";
+// import Loading from "./loading";
+import { toast } from "react-toastify";
+
+const MainWrapper = ({ children }) => {
+  const { setUser, setToken, isSecretary } = useAuth();
+
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const checkAuth = () => {
+    const token = localStorage.getItem("tech-token");
+    const storedUser = localStorage.getItem("user");
+
+    if (pathname === "/login" && token) {
+      router.push("/");
+      setIsLoading(false);
+      return true;
+    }
+
+    if (isSecretary) {
+      if (pathname.startsWith("/secretaries")) {
+        toast.error("شما دسترسی ورود به این بخش را ندارید");
+        router.push("/");
+        setIsLoading(false);
+        return false;
+      }
+    }
+
+    if (pathname !== "/login" && !token) {
+      router.push("/login");
+      setIsLoading(false);
+      return false;
+    }
+
+    if (storedUser) {
+      const userObject = JSON.parse(storedUser);
+
+      setUser(userObject);
+      setToken(token);
+    }
+
+    setIsLoading(false);
+    return true;
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }, [pathname, isSecretary]);
+
+  // if (isLoading) {
+  //   return <Loading />;
+  // }
+
+  return <>{children}</>;
+};
+
+export default MainWrapper;
